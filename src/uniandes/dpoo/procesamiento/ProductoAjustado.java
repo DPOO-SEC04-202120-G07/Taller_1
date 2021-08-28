5afb9bc0af5aba40a854310c48540728ef506446
camilo morillo <c.morillo@uniandes.edu.co>
2021-08-28 18:23:34 -0500

    Metodos clases producto

--- src/uniandes/dpoo/procesamiento/ProductoAjustado.java
+++ src/uniandes/dpoo/procesamiento/ProductoAjustado.java
@@ -6,5 +6,23 @@ public class ProductoAjustado implements Producto{
 	private ProductoMenu base;
 	private ArrayList<Ingrediente> agregados;
 	private ArrayList<Ingrediente> eliminados;
+	
+	//Metodo constructor
+	public ProductoAjustado(ProductoMenu base) {
+		this.base=base;
+	}
+
+	//Metodos
+	public int getPrecio() {
+		return base.getPrecio();
+	}
+	
+	public String getNombre() {
+		return base.getNombre();
+	}
+	
+	public String generarTextoFactura() {
+		return base.generarTextoFactura();
+	}
 
 }
