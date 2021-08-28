4c9b06da43fcf380b15d9c7aad0812fa0130f640
camilo morillo <c.morillo@uniandes.edu.co>
2021-08-28 17:38:37 -0500

    Mostrar opciones disponibles

--- src/uniandes/dpoo/consola/ConsolaHamburguesas.java
+++ src/uniandes/dpoo/consola/ConsolaHamburguesas.java
@@ -1,5 +1,6 @@
 package uniandes.dpoo.consola;
 import uniandes.dpoo.modelo.*;
+import java.util.Scanner;
 
 public class ConsolaHamburguesas {
 	//Atributos
@@ -7,11 +8,26 @@ public class ConsolaHamburguesas {
 	
 	//Metodo main
 	public static void main(String[] args) {
-		System.out.println("Inicio de ejecución de la aplicación");
+		System.out.println("BIENVENIDO A LA HAMBURGUESERIA.");
+		ConsolaHamburguesas interfaz = new ConsolaHamburguesas();
+		interfaz.mostrarOpciones();
+		Scanner input = new Scanner(System.in);
+		int opcionSeleccionada = input.nextInt();
+		interfaz.ejecutarOpcion(opcionSeleccionada);
 
 	}
 
 	//Metodos
+	public void mostrarOpciones() {
+		System.out.println("1. Mostrar el menú.");
+		System.out.println("2. Iniciar un nuevo pedido.");
+		System.out.println("3. Agregar un elemento a un pedido.");
+		System.out.println("4. Cerrar un pedido y guardar la factura.");
+		System.out.println("5. Consultar informacion de un pedido.");
+		System.out.println("Seleccione una de las anteriores opciones: ");
+		
+	}
+	
 	public void mostrarMenu() {
 		
 	}
