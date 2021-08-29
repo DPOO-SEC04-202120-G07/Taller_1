b6fdcabecc72047c840c70ecc774855e44bceb55
tadzio8425 <js.ortegar1@uniandes.edu.co>
2021-08-28 22:11:30 -0500

    Consola arrglada

--- src/uniandes/dpoo/consola/ConsolaHamburguesas.java
+++ src/uniandes/dpoo/consola/ConsolaHamburguesas.java
@@ -45,19 +45,19 @@ public class ConsolaHamburguesas {
 		ArrayList<Combo> combos = restaurante.getCombos();
 		
 		for (int i=0; i<menuBase.size(); i++) {
-<<<<<<< HEAD
+
 			System.out.println("Id:" + menuBase.get(i).getId()+ ") " + menuBase.get(i).getNombre()+" --- $"+menuBase.get(i).getPrecio()+"\n");
-=======
+
 			System.out.println(menuBase.get(i).getNombre()+" --- $"+menuBase.get(i).getPrecio());
->>>>>>> branch 'master' of https://github.com/DPOO-SEC04-202120-G07/Taller_1.git
+
 		}
 		
 		for (int i=0; i<combos.size(); i++) {
-<<<<<<< HEAD
+
 			System.out.println("Id:" + combos.get(i).getId()+ ") " + combos.get(i).getNombre()+" --- $"+combos.get(i).getPrecio()+"\n");
-=======
+
 			System.out.println(combos.get(i).getNombre()+" --- $"+combos.get(i).getPrecio());
->>>>>>> branch 'master' of https://github.com/DPOO-SEC04-202120-G07/Taller_1.git
+
 		}
 		
 	}
@@ -72,7 +72,7 @@ public class ConsolaHamburguesas {
 				String direccion = input_2.nextLine();
 				System.out.println("Ingrese el ID de su pedido: ");
 				int pedido = input_2.nextInt();
-				restaurante.IniciarPedido(nombre, direccion, pedido);
+				restaurante.IniciarPedido(nombre, direccion);
 		case 3: ;
 		case 4: ;
 		case 5: ;
